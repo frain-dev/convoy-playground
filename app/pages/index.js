e2940e78c04ddfd78963d2b6a3ad0f31c885d0fa
Pelumi Honey <pelumioni25@gmail.com>
2023-05-17 12:30:31 +0100

    feat: convoy playground UI

---
+++ app/pages/index.js
@@ -0,0 +1,8 @@
+function Index() {
+  return <div>
+    <p className="text-18 text-center">Convoy Playground</p>
+    <p className="text-center">A playground for you to receive and send out webhook events, test, debug and review webhook events; just like you will with Convoy </p>
+  </div>
+}
+
+export default Index;
