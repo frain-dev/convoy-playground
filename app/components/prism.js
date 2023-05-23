7d6eb4e3844c41b64ebbcd1ffd4e33731d9310ea
Pelumi Honey <pelumioni25@gmail.com>
2023-05-23 11:53:06 +0100

    update: update prism component

--- app/components/prism.js
+++ app/components/prism.js
@@ -15,38 +15,34 @@ const CodeRenderer = ({ title, language, code }) => {
     }, []);
 
     return (
-        <div className="border border-primary-25 rounded-8px mb-26px">
-            <h4 className="border-b border-primary-25 bg-primary-25 py-8px px-22px rounded-tl-8px rounded-tr-8px text-14 text-gray-600">
-                {title}
-            </h4>
+        <div>
+            <h4 className="py-8px text-12 text-gray-400">{title}</h4>
+            <div className="border border-primary-25 rounded-8px mb-26px">
+                <pre className={`${language} line-numbers`}>
+                    <code className={`${language} line-numbers`}>
+                        {JSON.stringify(code, null, 4).replaceAll(
+                            /"([^"]+)":/g,
+                            "$1:"
+                        )}
+                    </code>
+                </pre>
 
-            <pre className={`${language} line-numbers`}>
-                <code className={`${language} line-numbers`}>
-                    {JSON.stringify(code, null, 4).replaceAll(
-                        /"([^"]+)":/g,
-                        "$1:"
-                    )}
-                </code>
-            </pre>
-
-            {code && code.length > 300 && (
-                <div className="flex justify-end py-8px px-16px border-t border-primary-25">
-                    <button
-                        onClick={() =>
-                            showMorePayload
-                                ? setMorePayloadState(false)
-                                : setMorePayloadState(true)
-                        }
-                        className="text-12 font-medium text-primary-400 flex items-center"
-                    >
-                        {showMorePayload ? "Hide" : "Show more"} lines
-                        <img
-                            src="/angle-down-primary.svg"
-                            alt="angle-down icon"
-                        />
-                    </button>
-                </div>
-            )}
+                {code && (
+                    <div className="flex py-8px px-16px border-t border-primary-25">
+                        <button
+                            onClick={() =>
+                                showMorePayload
+                                    ? setMorePayloadState(false)
+                                    : setMorePayloadState(true)
+                            }
+                            className="text-12 font-medium text-primary-400 flex items-center"
+                        >
+                            <img src="/expand.svg" alt="expand icon" className="mr-10px" />
+                            {showMorePayload ? "Hide" : "Show more"} lines
+                        </button>
+                    </div>
+                )}
+            </div>
         </div>
     );
 };
