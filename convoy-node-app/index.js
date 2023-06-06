34af1a80b07a971fe4c1a6fb70c211ec7cd8fd34
Pelumi Honey <pelumioni25@gmail.com>
2023-06-06 11:40:11 +0100

    fix: fix proxy server

--- convoy-node-app/index.js
+++ convoy-node-app/index.js
@@ -1,24 +1,27 @@
 const express = require("express");
 const cors = require("cors");
 const proxy = require("express-http-proxy");
+require("dotenv").config();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT;
+const baseUrl = process.env.BASE_URL;
+
 app.use(express.json());
 app.use(cors());
 app.use(
-  "/",
-  proxy("http://localhost:5005", {
-    proxyReqBodyDecorator: (bodyContent, srcReq) => {
-      return JSON.stringify(srcReq.body);
-    },
-    proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
-      proxyReqOpts.headers["Content-Type"] = "application/json";
-      return proxyReqOpts;
-    },
-  })
+    "/",
+    proxy(baseUrl, {
+        proxyReqBodyDecorator: (bodyContent, srcReq) => {
+            return JSON.stringify(srcReq.body);
+        },
+        proxyReqOptDecorator: function (proxyReqOpts, srcReq) {
+            proxyReqOpts.headers["Content-Type"] = "application/json";
+            return proxyReqOpts;
+        },
+    })
 );
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
